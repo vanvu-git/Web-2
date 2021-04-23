34cb4e1a3e9d574043fa73b06173129c1f75b2a4
Vu <vanvu2329@gmail.com>
2021-04-23 19:56:06 +0700

    change all.php

--- all.php
+++ all.php
@@ -126,7 +126,7 @@
                     <button class="delete" onclick="myFunction('fix-<?php echo $row['id'];?>')" aria-label="close"></button>
                     </header>
                     <section class="modal-card-body">
-                    <form action="xulysua.php" method="POST" class="p-6 m-6 box" name="them_sp" enctype= "multipart/form-data">
+                    <form action="xulysua.php" method="POST"  name="them_sp" enctype= "multipart/form-data">
                       
                       <div class="field">
                           <label for="" class="label">Tên</label>

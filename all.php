f04af54054acad64a59041c53c5722f02080a8f9
Vu <vanvu2329@gmail.com>
2021-04-23 19:43:09 +0700

    change all.php

--- all.php
+++ all.php
@@ -117,8 +117,84 @@
                     </footer>
                 </div>
         </div>
-      <td><a class="button is-primary" href="<?php echo 'index.php?id=fix&idsp='.$row['id']?>"> Sửa</a></td>
-      
+        <td><button class="button is-primary" onclick="myFunction('<?php echo $row['id'];?>')">Sửa</button></td>
+        <div class="modal" id="<?php echo $row['id'];?>">
+                <div class="modal-background"></div>
+                <div class="modal-card">
+                    <header class="modal-card-head">
+                    <p class="modal-card-title">Sửa sản phẩm</p>
+                    <button class="delete" onclick="myFunction('<?php echo $row['id'];?>')" aria-label="close"></button>
+                    </header>
+                    <section class="modal-card-body">
+                    <form action="xulysua.php" method="POST" class="p-6 m-6 box" name="them_sp" enctype= "multipart/form-data">
+                      <h3 class="is-size-3 has-text-center">Sửa</h3>
+                      
+                      <div class="field" style="display:none;">
+                        <label for="" class="label">Id:</label>
+                        <div class="control">
+                            <input type="text" class="input" name="msp" value="<?php echo $row['id']; ?>" id="msp">
+                        </div>
+                        <div id="showUsernameError" class="has-text-danger"></div>
+                      </div>
+                      <div class="field">
+                      
+                          <label for="" class="label">Tên</label>
+                          <div class="control">
+                              <input type="text" class="input" name="ten">
+                          </div>
+                          <div id="showNameError" class="has-text-danger"></div>
+                      </div>
+
+                      <div class="field">
+                          <label for="" class="label">Đơn giá:</label>
+                          <div class="control">
+                              <input type="text" class="input" name="dg">
+                          </div>
+                          <div id="showNameError" class="has-text-danger"></div>
+                      </div>
+
+                      <div class="field">
+                        <label for="" class="label">Số lượng:</label>
+                        <div class="control">
+                            <input type="text" class="input" name="sl">
+                        </div>
+                        <div id="showNameError" class="has-text-danger"></div>
+                    </div>
+
+                      <div class="field">
+                        <div class="file has-name">
+                              <label class="file-label">
+                                <input class="file-input" type="file" name="file">
+                                <span class="file-cta">
+                                      <span class="file-icon">
+                                      <i class="fas fa-upload"></i>
+                                      </span>
+                                      <span class="file-label">
+                                            Choose a file…
+                                      </span>
+                                      </span>
+                                      <span class="file-name">
+                                            Hình ảnh
+                                </span>
+                              </label>
+                        </div>
+                      </div>
+
+
+                      <div class="field">
+                          <div class="control">
+                              <input type="submit" class="button" name="submit" value="Sửa">
+                          </div>
+                      </div>
+                    </form>
+
+                    </section>
+                    <footer class="modal-card-foot">
+                    <a   class="button is-primary" href="<?php echo 'xulyxoa.php?idsp='.$row['id']?>">Xóa</a>
+                    <button class="button" onclick="myFunction('<?php echo $row['id'];?>')">Cancel</button>
+                    </footer>
+                </div>
+        </div>
     </tr>
      <tr></tr>
   </tbody>

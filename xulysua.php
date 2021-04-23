5c1d389e0788fd3f7a21fa0fd4869be8552d89c7
Vu <vanvu2329@gmail.com>
2021-04-23 20:06:36 +0700

    change all.php

--- xulysua.php
+++ xulysua.php
@@ -11,9 +11,9 @@
     $idtl = $_POST["mtl"];
     $tsp = $_POST["ten"];
     $dg = $_POST["dg"];
-
+    $sl = $_POST["sl"];
     $src="images/" . $_FILES["file"]["name"];
-$sql = "UPDATE `sanpham` SET `id_theloai`='$idtl',`ten`='$tsp',`dongia`='$dg',`image_link`='$src' WHERE id='$msp'";
+$sql = "UPDATE `sanpham` SET `id_theloai`='$idtl',`ten`='$tsp',`dongia`='$dg',`image_link`='$src',`Soluong`='$sl' WHERE id='$msp'";
 echo $sql;
 mysqli_query($conn,$sql);
 mysqli_Close($conn);

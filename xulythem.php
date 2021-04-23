c0946cbb92000f5e12f6ed526fedc590050f3bdf
Vu <vanvu2329@gmail.com>
2021-04-23 19:21:37 +0700

    change xulythem.php

--- xulythem.php
+++ xulythem.php
@@ -3,15 +3,15 @@
     $idtl = $_POST["mtl"];
     $tsp = $_POST["ten"];
     $dg = $_POST["dg"];
-    
+    $sl = $_POST["sl"];
     $conn = new mysqli("localhost","root","","console-beta");
     if(!$conn)
     {
         echo"ket noi that bai";
     }
     $src= "images/" . $_FILES["file"]["name"];
-    $sql = "INSERT INTO `sanpham`(`id`, `id_theloai`, `ten`,`dongia`, `image_link`)
-     VALUES ( '$idsp' , '$idtl' , '$tsp' , '$dg' , '$src')";
+    $sql = "INSERT INTO `sanpham`(`id`, `id_theloai`, `ten`,`dongia`, `image_link`,'Soluong')
+     VALUES ( '$idsp' , '$idtl' , '$tsp' , '$dg' , '$src','$sl')";
     echo $sql;
     mysqli_query($conn,$sql);
  
